35ecff3e8f988d99811a7340b25402330ca668cf
Lara Rockenstein <lara.rockenstein@live.de>
2022-07-15 16:52:57 +0200

    intial setup and models

---
+++ models/Product.model.js
@@ -0,0 +1,21 @@
+const { Schema, model } = require("mongoose");
+
+// TODO: Please make sure you edit the user model to whatever makes sense in this case
+const productSchema = new Schema(
+  {
+    name: {
+        type: String,
+        required: true,
+    },
+    price: {
+        type: Number,
+    },
+    notes: String, 
+    image: String,
+    link: String
+  }
+);
+
+const Product = model("Product", productSchema);
+
+module.exports = Product;
---
+++ models/User.model.js
@@ -0,0 +1,26 @@
+const { Schema, model } = require("mongoose");
+
+// TODO: Please make sure you edit the user model to whatever makes sense in this case
+const userSchema = new Schema(
+  {
+    email: {
+      type: String,
+      required: [true, 'Email address is required.'],
+      lowercase: true,
+      trim: true,
+      unique: true,
+      match: [/^\S+@\S+\.\S+$/, 'Please use a valid email address.']
+      // unique: true -> Ideally, should be unique, but its up to you
+    },
+    passwordHash: {
+      type: String,
+      required: [true, 'Password is required.']
+  },
+    // this second object adds extra properties: `createdAt` and `updatedAt`
+    timestamps: true,
+}
+);
+
+const User = model("User", userSchema);
+
+module.exports = User;
