c4dd4bd0b52438d1b36d3b052807edbe88b6d0b1
Lisa Hilterhaus <lisa@hilterhaus.net>
2022-07-20 09:06:46 +0200

    minor changes and deleted test files

--- middleware/isAdmin.js
+++
@@ -1,8 +0,0 @@
-const isAdmin = (req, res, next) => {
-    if (req.session.user.role != "admin") {
-      return res.redirect("/login");
-    }
-    next();
-  };
-
-  module.exports = {isAdmin}
--- models/User.model.js
+++ models/User.model.js
@@ -3,7 +3,6 @@ const { Schema, model } = require("mongoose");
 // TODO: Please make sure you edit the user model to whatever makes sense in this case
 const userSchema = new Schema(
   {
-    name: String,
     email: {
       type: String,
       required: [true, 'Email address is required.'],
